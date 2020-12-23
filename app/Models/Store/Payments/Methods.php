1f22c1b7259c99ab352c93fbe9aa69c9d0e4837e
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-12-23 17:22:33 +0100

    wip:refactoring paymetns

--- app/Models/Store/Payments/Methods.php
+++ app/Models/Store/Payments/Methods.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Models\Store;
+namespace App\Models\Store\Payments;
 
 use App\Models\Store;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PaymentMethods extends Model
+class Methods extends Model
 {
     use HasFactory;
 
